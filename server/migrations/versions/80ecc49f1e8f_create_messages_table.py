7c83af078a73b2c4fa18ab1b546bca9b39a76144
Kelsey Roche <roche.kelsey.24@gmail.com>
2025-01-08 13:11:19 -0500

    complete lab

---
+++ server/migrations/versions/80ecc49f1e8f_create_messages_table.py
@@ -0,0 +1,24 @@
+"""Create messages table
+
+Revision ID: 80ecc49f1e8f
+Revises: 
+Create Date: 2025-01-08 13:07:20.904378
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '80ecc49f1e8f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/seed.py
+++ server/seed.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
 from random import choice as rc
-
 from faker import Faker
-
 from app import app
 from models import db, Message
 
@@ -14,11 +12,9 @@ if "Duane" not in usernames:
     usernames.append("Duane")
 
 def make_messages():
-
     Message.query.delete()
-    
-    messages = []
 
+    messages = []
     for i in range(20):
         message = Message(
             body=fake.sentence(),
@@ -27,7 +23,7 @@ def make_messages():
         messages.append(message)
 
     db.session.add_all(messages)
-    db.session.commit()        
+    db.session.commit()
 
 if __name__ == '__main__':
     with app.app_context():
